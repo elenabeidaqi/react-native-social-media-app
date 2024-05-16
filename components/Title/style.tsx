7122883b75401999792dc3e3549cb9fafaa55222
elenabeidaqi <elenabeidaqi@gmail.com>
2024-05-17 00:12:11 +0330

    :lipstick: add fonts and react-native-asset and Title component

---
+++ components/Title/style.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+const style = StyleSheet.create({
+    title : {
+        fontFamily : 'Inter',
+        fontSize : 24,
+        lineHeight : 29,
+        color : "#022150",
+        fontWeight : "600"
+    }
+})
+export default style;
