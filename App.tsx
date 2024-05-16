5f4dea1e0353a94e406fc4e9769b8386a82456a3
elenabeidaqi <elenabeidaqi@gmail.com>
2024-05-16 20:33:13 +0330

    :tada: initial app

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
